93acd0bffa641a6709d8fd2e7c225404881d3ae1
leandroArias1 <Wazzybtw@gmail.com>
2026-01-18 23:40:37 -0300

    listo para redeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
+
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://leandro-arias.vercel.app",
+  integrations: [sitemap()],
+});
